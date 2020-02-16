b3ff39bfaef6c6b709ec3ddeda0eee580e8a862c
anhnq <anhnq@tmh-techlab.vn>
2020-02-16 10:21:28 +0700

    make pretty profile

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -5,6 +5,7 @@
 import 'package:baby_garden_flutter/screen/base_state.dart';
 import 'package:baby_garden_flutter/generated/l10n.dart';
 import 'package:baby_garden_flutter/provider/get_list_provider.dart';
+import 'package:baby_garden_flutter/screen/profile/userInfor.dart';
 import 'package:baby_garden_flutter/util/resource.dart';
 import 'package:baby_garden_flutter/widget/svg_icon.dart';
 import 'package:flutter/cupertino.dart';
@@ -20,6 +21,11 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends BaseState<ProfileScreen> {
   final GetListProvider _getListProvider = GetListProvider();
 
+  final UserInfor userInfor = UserInfor(
+    username: "Quyền Anh đẹp trai",
+    mobilePhone: "0338305xxx",
+    joinDate: "13/2/2020",
+  );
   @override
   Widget buildWidget(BuildContext context) {
     final List<Map<String, String>> entries = <Map<String, String>>[
@@ -36,44 +42,56 @@ class _ProfileScreenState extends BaseState<ProfileScreen> {
 
     return Scaffold(
         appBar: getAppBar(title: S.of(context).myProfile),
-        body: Container(
-          child: ListView.builder(
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  // margin: const EdgeInsets.all(15.0),
-                  padding: const EdgeInsets.all(3.0),
-                  decoration: BoxDecoration(
-                      border: Border(
-                    bottom: BorderSide(
-                        width: 1,
-                        style: BorderStyle.solid,
-                        color: Color.fromRGBO(206, 206, 206, 1)),
-                  )),
-                  height: 50,
-                  child: Row(
-                    children: <Widget>[
-                      SvgIcon(
-                        '${entries[index]['icon']}.svg',
-                        color: (index == 0)
-                            ? ColorUtil.primaryColor
-                            : ColorUtil.black33,
-                        padding: EdgeInsets.only(left: 12, right: 15),
-                      ),
-                      Text(entries[index]['title'],
-                          style: TextStyle(
-                              color: (index == 0)
-                                  ? ColorUtil.primaryColor
-                                  : ColorUtil.black33)),
-                    ],
-                  ),
-                );
-              }),
-        ));
+        body: Column(children: <Widget>[
+          Container(
+              // user information
+              child: this.userInfor),
+          Container(
+            child: entriesWidget(entries),
+          ),
+        ]));
   }
 
   @override
   List<SingleChildWidget> providers() {
     return [ChangeNotifierProvider.value(value: _getListProvider)];
+  }
+
+  Widget entriesWidget(List<Map<String, String>> entries) {
+    return Container(
+      height: 350,
+      child: ListView.builder(
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              // margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border(
+                bottom: BorderSide(
+                    width: 1,
+                    style: BorderStyle.solid,
+                    color: Color.fromRGBO(206, 206, 206, 1)),
+              )),
+              height: 50,
+              child: Row(
+                children: <Widget>[
+                  SvgIcon(
+                    '${entries[index]['icon']}.svg',
+                    color: (index == 0)
+                        ? ColorUtil.primaryColor
+                        : ColorUtil.black33,
+                    padding: EdgeInsets.only(left: 12, right: 15),
+                  ),
+                  Text(entries[index]['title'],
+                      style: TextStyle(
+                          color: (index == 0)
+                              ? ColorUtil.primaryColor
+                              : ColorUtil.black33)),
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
