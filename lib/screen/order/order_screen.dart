37fd5a6ede760ab32f5ac23cf3c0c021266dbd58
nhapcs <phamnha0209.1995@gmail.com>
2020-02-21 18:51:16 +0700

    update order

--- lib/screen/order/order_screen.dart
+++ lib/screen/order/order_screen.dart
@@ -1,5 +1,6 @@
 import 'package:baby_garden_flutter/generated/l10n.dart';
 import 'package:baby_garden_flutter/screen/base_state.dart';
+import 'package:baby_garden_flutter/util/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:nested/nested.dart';
 
@@ -15,7 +16,12 @@ class _OrderState extends BaseState<OrderScreen> {
   Widget buildWidget(BuildContext context) {
     return Column(
       children: <Widget>[
-        getAppBar(title: S.of(context).order.toUpperCase(), hasBack: false)
+        getAppBar(title: S.of(context).order.toUpperCase(), hasBack: false),
+        Text(
+          S.of(context).my_order,
+          style: TextStyle(
+              color: ColorUtil.primaryColor, fontWeight: FontWeight.bold),
+        )
       ],
     );
   }
